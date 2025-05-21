41b0a014d88b1e53fd5f756ffdde3869e5ea1e9c
andri <andriymedvid@gmail.com>
2025-05-22 01:14:53 +0300

    Fixed build file

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ plugins {
     alias(libs.plugins.compose.compiler) apply false
     alias(libs.plugins.ksp) apply false
     alias(libs.plugins.google.services) apply false
-    alias(libs.plugins.firebase.crashlytics) apply false
+//    alias(libs.plugins.firebase.crashlytics) apply false
     alias(libs.plugins.kotlinx.kover)
     jacoco
 }
