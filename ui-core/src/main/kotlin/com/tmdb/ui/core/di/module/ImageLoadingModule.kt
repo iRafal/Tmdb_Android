92cd56cdec71a77ba8b151374c9a44cdc449cce7
andri <andriymedvid@gmail.com>
2025-07-02 23:34:49 +0300

    Updated libraries. Fixed warnings. Code improvements.

--- ui-core/src/main/kotlin/com/tmdb/ui/core/di/module/ImageLoadingModule.kt
+++ ui-core/src/main/kotlin/com/tmdb/ui/core/di/module/ImageLoadingModule.kt
@@ -10,10 +10,6 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
-import okhttp3.Interceptor
-import okhttp3.OkHttpClient
-import okhttp3.logging.HttpLoggingInterceptor
-import okhttp3.logging.HttpLoggingInterceptor.Level.BODY
 import javax.inject.Qualifier
 
 @InstallIn(SingletonComponent::class)
@@ -27,9 +23,7 @@ object ImageLoadingModule {
         @ApplicationContext context: Context,
     ): ImageLoader = ImageLoader.Builder(context)
         .logger(if (BuildConfig.DEBUG) DebugLogger() else null)
-        .components {
-            add(OkHttpNetworkFetcherFactory())
-        }
+        .components { add(OkHttpNetworkFetcherFactory()) }
         .build()
 
 //    @[Qualifier Retention(AnnotationRetention.BINARY)]
