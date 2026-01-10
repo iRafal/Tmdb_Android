eb5d9f9ca35301a5436b160ec1262935b7c4650e
andri <andriymedvid@gmail.com>
2026-01-10 18:20:13 +0200

    Removed deprecated realm

--- data/source/local/impl-realm/consumer-rules.pro
+++
@@ -1,5 +0,0 @@
--keep,allowobfuscation class com.tmdb.data.source.local.implRealm.**
--keep,allowobfuscation class com.tmdb.data.source.local.implRealm.**$* {
-    <fields>;
-    <init>(...);
-}
--- data/source/local/impl-realm/src/main/kotlin/com/tmdb/data/source/local/impl/realm/di/component/HasLocalDataSources.kt
+++
@@ -1,8 +0,0 @@
-package com.tmdb.data.source.local.impl.realm.di.component
-
-import com.tmdb.data.source.local.contract.MovieLocalDataSource
-
-
-interface HasLocalDataSources {
-    val movieLocalDataSource: MovieLocalDataSource
-}
